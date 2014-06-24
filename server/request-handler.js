c7d2e80b9bc029561ced1096ea2086e85ee44c99
lawmay <lmay01@gmail.com>
2014-06-23 20:28:04 -0700

    finish basic requirements

--- server/request-handler.js
+++ server/request-handler.js
@@ -5,22 +5,63 @@
  * this file and include it in basic-server.js so that it actually works.
  * *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html. */
 
-var handleRequest = function(request, response) {
+var count = 0;
+
+var storage = [];
+
+
+var requestHandler = {};
+
+requestHandler.handler = function(request, response) {
   /* the 'request' argument comes from nodes http module. It includes info about the
   request - such as what URL the browser is requesting. */
 
   /* Documentation for both request and response can be found at
    * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html */
-
+  var body = {
+    results: []
+  };
   console.log("Serving request type " + request.method + " for url " + request.url);
 
   var statusCode = 200;
+  // console.log("HEADER", request.headers);
+  console.log("URL", request.url);
+  if (request.method === 'POST') {
+    statusCode = 201;
+    request.on('data', function(data) {
+      data = JSON.parse(data);
+      storage.push(createMessage(data));
+    });
+  } else if (request.method === 'GET') {
+    if (request.url === '/arglebargle') {
+      statusCode = 404;
+    }
 
+
+    //Examine URL for Query
+    var filter = examineURL(request.url);
+    console.log("FILTER", filter);
+
+    for (var i = 0; i < storage.length; i++) {
+      var valid = true;
+      for (var key in filter) {
+        if (filter[key] !== storage[i][key]) {
+          valid = false;
+          break;
+        }
+      }
+      if (valid) {
+        body.results.push(storage[i]);
+      }
+    }
+  }
   /* Without this line, this server wouldn't work. See the note
    * below about CORS. */
+
   var headers = defaultCorsHeaders;
 
-  headers['Content-Type'] = "text/plain";
+  // headers['Content-Type'] = "text/plain";
+  headers['Content-Type'] = "application/json";
 
   /* .writeHead() tells our server what HTTP status code to send back */
   response.writeHead(statusCode, headers);
@@ -29,7 +70,8 @@ var handleRequest = function(request, response) {
    * anything back to the client until you do. The string you pass to
    * response.end() will be the body of the response - i.e. what shows
    * up in the browser.*/
-  response.end("Hello, World!");
+
+  response.end(JSON.stringify(body));
 };
 
 /* These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -43,3 +85,30 @@ var defaultCorsHeaders = {
   "access-control-allow-headers": "content-type, accept",
   "access-control-max-age": 10 // Seconds.
 };
+
+function createMessage(data) {
+  count++;
+  return {
+    'username': data.username,
+    'objectId': count,
+    'message': data.text,
+    'createdAt': new Date(),
+    'updatedAt': new Date(),
+    'roomname': data.roomname
+  };
+}
+
+module.exports = requestHandler;
+
+function examineURL(url) {
+  var obj;
+  url = decodeURIComponent(url);
+  console.log("URL in EXAMINE", url);
+  if (url[url.length - 1] === '}') {
+    obj = JSON.parse(url.substring(url.lastIndexOf('{')));
+  }
+  console.log("OBJECT", obj)
+  return obj;
+}
+
+
